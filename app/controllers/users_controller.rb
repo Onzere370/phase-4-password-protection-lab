c4250273c12bc85318ade3050aeb0f20b479b9db
cynthia <cynthiakagz@gmail.com>
2023-09-02 21:38:27 +0300

    commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+      
+        if user.save
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+    
+    def show_current_user
+        if current_user
+          render json: current_user, status: :ok
+        else
+          head :unauthorized
+        end
+    end
+    
+      private
+    
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users/me', to: 'users#show_current_user'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
